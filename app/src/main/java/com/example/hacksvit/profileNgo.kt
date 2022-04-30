3ecc4051baeff1009bd64c68db0c8faeab346050
Karan <karan.savaliya01@gmail.com>
2022-04-30 19:21:54 +0530

    Updated UI

--- app/src/main/java/com/example/hacksvit/profileNgo.kt
+++ app/src/main/java/com/example/hacksvit/profileNgo.kt
@@ -1,15 +1,36 @@
 package com.example.hacksvit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class profileNgo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_ngo)
 
-
-
+        val Edit_pofile_ngo  = findViewById<ImageButton>(R.id.edit_profile_ngo)
+        Edit_pofile_ngo.setOnClickListener {
+            startActivity(Intent(this, edit_Profile_Ngo::class.java))
+        }
+        val donors_contributors  = findViewById<ImageButton>(R.id.donors_contributors)
+        donors_contributors.setOnClickListener {
+            startActivity(Intent(this, ngoLoginPage::class.java))
+        }
+        val add_campaigns  = findViewById<ImageButton>(R.id.add_campaigns)
+        add_campaigns.setOnClickListener {
+            startActivity(Intent(this, addcampaigns::class.java))
+        }
+        val logout_ngo  = findViewById<ImageButton>(R.id.logout_ngo)
+        logout_ngo.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(this, signInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or (Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
 
     }
 }
